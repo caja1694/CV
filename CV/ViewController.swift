78278da3962425d69130756b4c0949d0eb015e1d
Jacob Carlquist <Jake@wlanguest14249.eduroam.hj.se>
2019-02-06 09:43:41 +0100

    Initial Commit

---
+++ CV/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CV
+//
+//  Created by Jacob Carlquist on 2019-02-06.
+//  Copyright © 2019 Jacob Carlquist. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ CVTests/CVTests.swift
@@ -0,0 +1,34 @@
+//
+//  CVTests.swift
+//  CVTests
+//
+//  Created by Jacob Carlquist on 2019-02-06.
+//  Copyright © 2019 Jacob Carlquist. All rights reserved.
+//
+
+import XCTest
+@testable import CV
+
+class CVTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
